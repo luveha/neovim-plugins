53e64c21fb128e3db304dd9d527f4fb72214e46d
Lucas <lucasveha@gmail.com>
2026-05-24 01:08:59 +0200

    ordered and ai fixed something related to fedora

--- init.lua
+++ init.lua
@@ -1,60 +1,63 @@
+-- ============================================================
+-- Leader key
+-- ============================================================
+
 vim.g.mapleader = " "
 
+
+-- ============================================================
+-- Editor Settings
+-- ============================================================
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- ============================================================
+-- Plugins
+-- ============================================================
+
 vim.pack.add({
-    'https://github.com/nvim-treesitter/nvim-treesitter',
-    'https://github.com/nvim-mini/mini.nvim',
-    'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  -- Syntax / UI
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+  "https://github.com/nvim-mini/mini.nvim",
+  "https://github.com/MeanderingProgrammer/render-markdown.nvim",
 
-    -- completion
-    'https://github.com/hrsh7th/nvim-cmp',
-    'https://github.com/hrsh7th/cmp-cmdline',
-    'https://github.com/hrsh7th/cmp-path',
-    'https://github.com/hrsh7th/cmp-buffer',
+  -- Completion
+  "https://github.com/hrsh7th/nvim-cmp",
+  "https://github.com/hrsh7th/cmp-cmdline",
+  "https://github.com/hrsh7th/cmp-path",
+  "https://github.com/hrsh7th/cmp-buffer",
 
-    -- terminal for lazygit
-    'https://github.com/akinsho/toggleterm.nvim',
+  -- Terminal
+  "https://github.com/akinsho/toggleterm.nvim",
 })
 
 vim.pack.add({
+  -- File tree
   {
-    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
-    version = vim.version.range('3')
+    src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+    version = vim.version.range("3"),
   },
-  -- dependencies
+
+  -- Neo-tree dependencies
   "https://github.com/nvim-lua/plenary.nvim",
   "https://github.com/MunifTanjim/nui.nvim",
-  -- optional, but recommended
   "https://github.com/nvim-tree/nvim-web-devicons",
 })
 
-require('render-markdown').setup({})
+
+-- ============================================================
+-- Render Markdown
+-- ============================================================
+
+require("render-markdown").setup({})
+
+
+-- ============================================================
+-- Neo-tree
+-- ============================================================
+
 require("neo-tree").setup({})
-
-local cmp = require('cmp')
-
-cmp.setup({
-    sources = {
-        { name = 'buffer' },
-    },
-})
-
--- ":" command completion
-cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = 'path' }
-    }, {
-        { name = 'cmdline' }
-    })
-})
-
--- "/" and "?" search completion
-cmp.setup.cmdline({ '/', '?' }, {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = 'buffer' }
-    }
-})
 
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
@@ -65,8 +68,44 @@ vim.api.nvim_create_autocmd("VimEnter", {
   end,
 })
 
+
+-- ============================================================
+-- Completion
+-- ============================================================
+
+local cmp = require("cmp")
+
+cmp.setup({
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+cmp.setup.cmdline(":", {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
+})
+
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = "buffer" },
+  },
+})
+
+
+-- ============================================================
+-- Terminal / LazyGit
+-- ============================================================
+
 require("toggleterm").setup({
   direction = "float",
+  close_on_exit = true,
+  shell = vim.o.shell,
   float_opts = {
     border = "rounded",
   },
@@ -78,16 +117,15 @@ local lazygit = Terminal:new({
   cmd = "lazygit",
   hidden = true,
   direction = "float",
-  close_on_exit = false,
-  dir = "git_dir",
+  close_on_exit = true,
   float_opts = {
     border = "rounded",
   },
-  on_open = function()
-    vim.cmd("startinsert!")
-  end,
 })
 
 vim.keymap.set("n", "<leader>gg", function()
+  vim.cmd("silent! write")
   lazygit:toggle()
-end, { desc = "Open LazyGit" })
+end, {
+  desc = "Open LazyGit",
+})
