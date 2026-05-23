a041eb4e47cb86ff2b9f6c2dfedf47a35015b482
Lucas <lucasveha@gmail.com>
2026-05-24 01:01:27 +0200

    added lazygit

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 vim.pack.add({
     'https://github.com/nvim-treesitter/nvim-treesitter',
     'https://github.com/nvim-mini/mini.nvim',
@@ -8,9 +10,25 @@ vim.pack.add({
     'https://github.com/hrsh7th/cmp-cmdline',
     'https://github.com/hrsh7th/cmp-path',
     'https://github.com/hrsh7th/cmp-buffer',
+
+    -- terminal for lazygit
+    'https://github.com/akinsho/toggleterm.nvim',
+})
+
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range('3')
+  },
+  -- dependencies
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  -- optional, but recommended
+  "https://github.com/nvim-tree/nvim-web-devicons",
 })
 
 require('render-markdown').setup({})
+require("neo-tree").setup({})
 
 local cmp = require('cmp')
 
@@ -37,3 +55,39 @@ cmp.setup.cmdline({ '/', '?' }, {
         { name = 'buffer' }
     }
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("neo-tree.command").execute({
+      toggle = false,
+      position = "left",
+    })
+  end,
+})
+
+require("toggleterm").setup({
+  direction = "float",
+  float_opts = {
+    border = "rounded",
+  },
+})
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  close_on_exit = false,
+  dir = "git_dir",
+  float_opts = {
+    border = "rounded",
+  },
+  on_open = function()
+    vim.cmd("startinsert!")
+  end,
+})
+
+vim.keymap.set("n", "<leader>gg", function()
+  lazygit:toggle()
+end, { desc = "Open LazyGit" })
